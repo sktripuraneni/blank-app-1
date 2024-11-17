0d505fa4a3edc94a774a33f7ecb8dce48cafc2c7
sktripuraneni <sktripuraneni@gmail.com>
2024-11-17 02:23:29 +0000

    ultralytics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from ultralytics import solutions
 
+solutions.inference()
+
+### Make sure to run the file using command `streamlit run <file-name.py>`
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+#sudo apt-get install python3-opencv
+#pip install ultralytics
+# (not required) pip install lapx
